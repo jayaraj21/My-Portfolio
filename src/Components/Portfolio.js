ee4c38e747b64b1fd541b5e7a9f1a2229228ea8e
jay z <jayaraj.vis3@gmail.com>
2020-08-10 01:52:04 +0530

    updating links

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,16 +4,16 @@ class Portfolio extends Component {
   render() {
 
     if(this.props.data){
-      var works = this.props.data.works.map(function(works){
-        var projectImage = 'images/portfolio/'+works.image;
-        return <div key={works.title} className="columns portfolio-item">
+      var projects = this.props.data.projects.map(function(projects){
+        var projectImage = 'images/portfolio/'+projects.image;
+        return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
-            <a href={works.url} title={works.title}>
-               <img alt={works.title} src={projectImage} />
+            <a href={projects.url} title={projects.title}>
+               <img alt={projects.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{works.title}</h5>
-                     <p>{works.category}</p>
+                 <h5>{projects.title}</h5>
+                     <p>{projects.category}</p>
                   </div>
                 </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
@@ -59,8 +59,8 @@ class Portfolio extends Component {
         
       </div>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {works}
+            <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
+                {projects}
             </div>
 
             
@@ -78,7 +78,7 @@ class Portfolio extends Component {
 
 </div>
 
-<div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+<div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
     {works}
 </div>
 
